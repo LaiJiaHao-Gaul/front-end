bbe3286c6579ce17ae778074e78b5cd30fe98c01
nunu <787071638@qq.com>
2021-05-14 13:50:29 +0800

    调整目录结构 新增Webpack和Babel相关内容

--- JavaScript/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-
-//     this.name = name;
-//     this.age = age;
-//     return {name:"doge",age:5};
-// }
-// let dog = new Dog("cheems",3);
-// console.log(dog); // { name: 'doge', age: 5 }
-// console.log(dog.__proto__ === Dog.prototype) // false
--- demo.js
+++
@@ -1,7 +0,0 @@
-function Person(){
-    this.age=10
-    this.name='lily'
-}
-var a = new Person()
-var b = new Person()
-console.log(a.__proto__.age)
