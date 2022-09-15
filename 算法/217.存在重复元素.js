1a8f0835b051cd6cbfced7fe962b550abc82da16
前端-赖嘉豪 <13543474433@139.cn>
2022-09-15 23:59:27 +0800

    存在重复元素-算法

---
+++ 算法/217.存在重复元素.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=217 lang=javascript
+ *
+ * [217] 存在重复元素
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+//  var containsDuplicate = function(nums) {
+//     for(let i=0;i<nums.length;i++){
+//         for(let j=i+1;i<nums.length;j++){
+//             console.log('i\n',i)
+//             console.log('j\n',j)
+//             if(nums[i]===nums[j]){
+//                 return true
+//             }
+//         }
+//     }
+//     return false
+// };
+var containsDuplicate = function(nums) {
+    nums.sort((a,b)=>a-b)
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]===nums[i+1]){
+            return true
+        }
+    }
+    return false
+};
+// @lc code=end
+
+
+// @after-stub-for-debug-begin
+module.exports = containsDuplicate;
+// @after-stub-for-debug-end
